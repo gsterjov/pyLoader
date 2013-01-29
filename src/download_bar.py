d50fff5566758b45166187d59d216f0fca97c2f4
Goran Sterjov <goran.sterjov@gmail.com>
2013-01-30 08:50:56 +1100

    Active downloads now update with the polling.

--- src/download_bar.py
+++ src/download_bar.py
@@ -18,8 +18,64 @@
 
 from gi.repository import Gtk, GLib, Pango, Gio
 
+import os
 import mimetypes
 import utils
+
+
+class DownloadItem (Gtk.HBox):
+	'''
+	An item in the active downloads bar
+	'''
+
+	def __init__ (self, link, status):
+		'''
+		Constructor
+		'''
+		Gtk.HBox.__init__ (self, False, 10)
+
+		self.link = link
+		self.status = status
+
+		mime, encoding = mimetypes.guess_type (link.name)
+
+		box = Gtk.HBox (False, 10)
+		details = Gtk.VBox (True, 0)
+
+		image = Gtk.Image ()
+		if mime:
+			icon = Gio.content_type_get_icon (mime)
+			image.set_from_gicon (icon, Gtk.IconSize.DIALOG)
+		else:
+			image.set_from_stock (Gtk.STOCK_FILE, Gtk.IconSize.DIALOG)
+
+		self.name = Gtk.Label()
+		self.name.set_alignment (0, 0.5)
+		self.name.set_ellipsize (Pango.EllipsizeMode.END)
+		self.name.set_max_width_chars (25)
+
+		self.progress = Gtk.ProgressBar()
+
+		self.speed = Gtk.Label()
+		self.speed.set_alignment (0, 0.5)
+		self.speed.set_ellipsize (Pango.EllipsizeMode.END)
+		self.speed.set_max_width_chars (25)
+
+		details.pack_start (self.name, False, True, 0)
+		details.pack_start (self.progress, False, True, 0)
+		details.pack_start (self.speed, False, True, 0)
+
+		self.pack_start (image, False, False, 0)
+		self.pack_start (details, True, True, 0)
+
+		self.update()
+
+
+	def update (self):
+		self.name.set_markup ("<small>{0}</small>".format (self.link.name))
+		self.progress.set_fraction (self.status.percent / 100.0)
+		self.speed.set_markup ("<small>{0}/s</small>".format (utils.format_size(self.status.speed)))
+
 
 
 class DownloadBar (object):
@@ -33,11 +89,14 @@ class DownloadBar (object):
 		'''
 		self.client = client
 
+		self.items = []
+
 		# load the main download box
 		self.downloads = builder.get_object ("downloads")
 		
 		# connect to server events
 		client.on_active_added += self.__on_active_added
+		client.on_active_changed += self.__on_active_changed
 	
 	
 	def show_connect_window (self):
@@ -58,38 +117,16 @@ class DownloadBar (object):
 		for package in self.client._queue_cache.itervalues():
 			for link in package.links:
 				if link.id == status.id:
-					found_link = link
-		
-		mime, encoding = mimetypes.guess_type (found_link.name)
-		icon = Gio.content_type_get_icon (mime)
+					item = DownloadItem (link, status)
+					self.items.append (item)
 
-		box = Gtk.HBox (False, 10)
-		details = Gtk.VBox (True, 0)
+					self.downloads.pack_start (item, False, True, 0)
+					self.downloads.show_all()
 
-		image = Gtk.Image ()
-		image.set_from_gicon (icon, Gtk.IconSize.DIALOG)
 
-		label = Gtk.Label()
-		label.set_markup ("<small>{0}</small>".format (found_link.name))
-		label.set_alignment (0, 0.5)
-		label.set_ellipsize (Pango.EllipsizeMode.END)
-		label.set_max_width_chars (25)
-
-		progress = Gtk.ProgressBar()
-		progress.set_fraction (status.percent / 100.0)
-
-		speed = Gtk.Label()
-		speed.set_markup ("<small>{0}/s</small>".format (utils.format_size(status.speed)))
-		speed.set_alignment (0, 0.5)
-		speed.set_ellipsize (Pango.EllipsizeMode.END)
-		speed.set_max_width_chars (25)
-
-		details.pack_start (label, False, True, 0)
-		details.pack_start (progress, False, True, 0)
-		details.pack_start (speed, False, True, 0)
-
-		box.pack_start (image, False, False, 0)
-		box.pack_start (details, True, True, 0)
-
-		self.downloads.pack_start (box, False, True, 0)
-		self.downloads.show_all()
+	def __on_active_changed (self):
+		'''
+		Handler to update items in the download bar
+		'''
+		for item in self.items:
+			item.update()
