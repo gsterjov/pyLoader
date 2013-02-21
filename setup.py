75618b399ebde2166be27e6789ec560fb2410c5a
Goran Sterjov <goran.sterjov@gmail.com>
2013-02-21 21:03:45 +1100

    Installation now adds the gschema and compiles it.

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
+from subprocess import Popen, PIPE
 from setuptools import setup, find_packages
 
 from distutils.core import setup
 from distutils.command.install import install as _install
+from distutils.command.install_data import install_data as _install_data
 
 
 class post_install (_install):
 	def run (self):
 		_install.run (self)
+
+
+class install_data (_install_data):
+	def run (self):
+		_install_data.run (self)
+		command = ("glib-compile-schemas", "/usr/share/glib-2.0/schemas/")
+		print (" ".join (command))
+		Popen (command, stdout=PIPE, stderr=PIPE).communicate()
 
 
 setup(
@@ -25,14 +35,24 @@ setup(
 
 
 	packages = find_packages (exclude=["tests.*", "tests"]),
-	package_data = {"pyloader": ["ui/*.xml"]},
-	zip_safe = False,
+
+	package_data = {
+		"pyloader": ["ui/*.xml"]
+	},
+
+	data_files = [
+		('/usr/share/glib-2.0/schemas', ['data/org.pyLoader.gschema.xml'])
+	],
 
 	entry_points = {
 		'console_scripts': ['pyloader = pyloader.window:start']
 	},
 
 	install_requires = ['thrift'],
+	zip_safe = False,
 
-	cmdclass = {"install": post_install},
+	cmdclass = {
+		"install": post_install,
+		"install_data": install_data,
+	},
 )
